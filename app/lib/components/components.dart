a710b6e595990169272ca50f40462906628b3c6c
sonpt17 <sonphamtrung17@gmail.com>
2025-09-11 09:51:36 +0700

    feat: Add AppTypingTextStream component for animated text display in home tab

--- app/lib/components/components.dart
+++ app/lib/components/components.dart
@@ -1,6 +1,7 @@
 //GENERATED BARREL FILE
 export 'app_button.dart';
 export 'app_image.dart';
+export 'app_typing_text_stream.dart';
 export 'base_app_bar.dart';
 export 'gradient_border_container.dart';
 export 'popup/common_dialog.dart';
